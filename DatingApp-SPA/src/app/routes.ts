0fbd6dca25e30ecd06093becd060fbb8419d25ca
Jitesh Kumar <jiteshj.kumar@gmail.com>
2019-06-11 20:00:01 +0530

    Adding Member Detailed View

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     {path: '', component: HomeComponent },
@@ -12,6 +13,7 @@ export const appRoutes: Routes = [
         canActivate: [AuthGuard],
         children: [
             {path: 'members', component: MemberListComponent },
+            {path: 'members/:id', component: MemberDetailComponent },
             {path: 'messages', component: MessagesComponent },
             {path: 'lists', component: MemberListComponent }
         ]
